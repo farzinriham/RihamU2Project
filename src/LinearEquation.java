41f4d9c0b2b382c39a197b70349358128707950c
BT_1N3_11 <no@gmail.com>
2023-10-16 08:54:49 -0400

    distance LinearEquation class updated 8:54 pm 10/16/2023

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private int xOne;
+    private int xTwo;
+    private int yOne;
+    private int yTwo;
+
+    public LinearEquation (int xOne,int xTwo,int yOne,int yTwo) {
+        xOne = this.xOne;
+        xTwo = this.xTwo;
+        yOne = this.yTwo;
+        yTwo = this.yTwo;
+    }
+
+    public double distance () {
+        double xDiff = Math.pow((xTwo-xOne), 2);
+        double yDiff = Math.pow((yTwo-yOne), 2);
+        double fin = Math.sqrt((xDiff + yDiff));
+        return fin;
+    }
 }
