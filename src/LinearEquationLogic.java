f8942efb658a1bb3bdbd251b6918dbae43f2ce2f
Farzin Riham <friham07@Gmail.com>
2023-10-30 19:06:10 -0400

    LinearEquationLogic edited 6:45pm October 30 2023

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -10,6 +10,8 @@ public class LinearEquationLogic {
     private int y1;
     private int y2;
 
+    /* Getting coordinates: Please remember to use these in start() method */
+
     private void getCoordinates1 () {
         System.out.print("Welcome to Equation Calculator! Please enter the first coordinate /example: (6, -2)/: ");
         coordinates1 = scan.nextLine();
@@ -20,8 +22,31 @@ public class LinearEquationLogic {
         coordinates2 = scan.nextLine();
     }
 
-    private void getX1 () { //USE THE INDEX OF COMMAAAAAAA, also note that coordinates can be like (x, y), and don't forget negative numbers exist!!!!
+    /* Getting x and y values: Please remember to use these in start() method */
+
+    private void getX1 () { //USE THE INDEX OF COMMAAAAAAA, also note that the coordinates are be like (x, y) with space after comma, and don't forget negative numbers exist!!!!
+        int idxComma = coordinates1.indexOf(",");
+        String xString = coordinates1.substring(1, idxComma);
+        x1 = Integer.parseInt(xString);
+    }
+
+    private void getY1 () { //USE THE INDEX OF COMMAAAAAAA, also note that the coordinates are be like (x, y) with space after comma, and don't forget negative numbers exist!!!!
         int lengthC1 = coordinates1.length();
         int idxComma = coordinates1.indexOf(",");
+        String yString = coordinates1.substring(idxComma+2, lengthC1-1);
+        y1 = Integer.parseInt(yString);
+    }
+
+    private void getX2 () { //USE THE INDEX OF COMMAAAAAAA, also note that the coordinates are be like (x, y) with space after comma, and don't forget negative numbers exist!!!!
+        int idxComma = coordinates2.indexOf(",");
+        String xString = coordinates2.substring(1, idxComma);
+        x2 = Integer.parseInt(xString);
+    }
+
+    private void getY2 () { //USE THE INDEX OF COMMAAAAAAA, also note that the coordinates are be like (x, y) with space after comma, and don't forget negative numbers exist!!!!
+        int lengthC2 = coordinates2.length();
+        int idxComma = coordinates2.indexOf(",");
+        String yString = coordinates2.substring(idxComma+2, lengthC2-1);
+        y2 = Integer.parseInt(yString);
     }
 }
