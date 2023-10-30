fa993dd033b80f798d8c140b9da4b2c8655b4c67
Farzin Riham <friham07@Gmail.com>
2023-10-30 18:41:50 -0400

    i hate life edited 5:52pm October 30 2023

---
+++ src/LinearEquationLogic.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LinearEquationLogic {
+    Scanner scan = new Scanner(System.in); //only logic should take in user input!
+
+    private String coordinates1;
+    private String coordinates2;
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    private void getCoordinates1 () {
+        System.out.print("Welcome to Equation Calculator! Please enter the first coordinate /example: (6, -2)/: ");
+        coordinates1 = scan.nextLine();
+    }
+
+    private void getCoordinates2 () {
+        System.out.print("Please enter the second coordinate /example: (6, -2)/: ");
+        coordinates2 = scan.nextLine();
+    }
+
+    private void getX1 () { //USE THE INDEX OF COMMAAAAAAA
+        int lengthC1 = coordinates1.length();
+
+    }
+}
